adbc77a050a2a2b3b4d1aa9985bcea152c440186
hecnabae <hector.navarro@13genius.com>
2023-11-24 20:57:59 +0100

    FIRST COMMIT

---
+++ src/main/java/com/anymetrik/opclogger/OpcloggerApplication.java
@@ -0,0 +1,13 @@
+package com.anymetrik.opclogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpcloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpcloggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anymetrik/opclogger/OpcloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anymetrik.opclogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpcloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
